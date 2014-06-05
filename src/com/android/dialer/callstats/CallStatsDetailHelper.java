060830dd312762f032b21dd041106f9a891bb8da
Ethan Chen <intervigil@gmail.com>
2014-06-05 15:49:19 -0700

    Dialer: Correct geocode resource typo
    Change-Id: I805b775d9655b73457bc8f01cff9b5417aa3f209

--- src/com/android/dialer/callstats/CallStatsDetailHelper.java
+++ src/com/android/dialer/callstats/CallStatsDetailHelper.java
@@ -69,7 +69,7 @@ public class CallStatsDetailHelper {
             nameText = displayNumber;
             if (TextUtils.isEmpty(details.geocode)
                     || mPhoneNumberUtilsWrapper.isVoicemailNumber(details.number)) {
-                numberText = "";
+                numberText = mResources.getString(R.string.call_log_empty_geocode);
             } else {
                 numberText = details.geocode;
             }
